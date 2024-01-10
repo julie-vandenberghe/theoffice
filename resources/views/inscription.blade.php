28b6e936d459426b5812cd9f782db694fb93eafd
Solangèle <angedespretz@gmail.com>
2024-01-10 14:29:14 +0100

    users

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Inscription</title>
 </head>
 <body>
+    <h1> Inscription</h1>
+
+    <div>
+        <label for="email">Email</label>
+        <input type="text" pattern=".+@example\.com" size="30" placeholder="toto@gmail.com" required>
+    </div>
+
+    <div>
+        <label for="passeword">Mot de passe</label>
+        <input type="text" minlength="8" required>
+    </div>
+    <div>
+        <label for="passeword_confirm">Confirmation du mot de passe</label>
+        <input type="text" minlength="8" required>
+    </div>
+
+    <div>
+        <input type="submit" value="S'inscrire">
+    </div>
+
+
     
 </body>
 </html>
